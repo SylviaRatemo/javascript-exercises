8f5d0c0a771c336332027ff048b938b552092959
ratemosly <ratemosylvia@gmail.com>
2022-10-25 18:37:21 +0300

    completed ex5, ex6, and ex7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,20 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    if (num1 < 0 || num2 < 0 || isNaN(num1)){
-        return "ERROR";
-    } else {
-        if (num1 < num2) {
-            for (let i = num1; i <= num2; i++){
-                sum += i;
-            }
-            return sum;
-        } else if (num1 > num2) {
-            for (let i = num2; i <= num1; i++){
-                sum += i;
-            }
-            return sum;
-        }
-    } 
+    if (num1 < 0 || num2 < 0){
+        return "ERROR"
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    if (num1 > num2){
+        const count = num2; 
+        num2 = num1;
+        num1 = count;
+    }
+    for (let i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
 }
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel = ((far - 32) * 5) / 9;
+  if (cel - Math.floor(cel) !== 0){
+    return parseFloat(cel.toFixed(1));
+  }
+  return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far = ((cel * 9) / 5) + 32;
+  if (far - Math.floor(far) !== 0) {
+    return parseFloat(far.toFixed(1));
+  }
+  return far;
 };
 
 // Do not edit below this line
